3e78435c4b50cfd8e3700f71b6976385da182531
Jeniffer <jeniqf@gmail.com>
2020-06-27 16:06:02 +0100

    Update

---
+++ euromilhoes/EuroNumeros.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euromilhoes
+{
+    public class EuroNumeros
+    {
+        private int[] chave;
+        private int[] estrelas;
+        public EuroNumeros()
+        {
+            this.chave = new int[5];
+            this.estrelas = new int[2];
+        }
+
+        public int[] getChave()
+        {
+            return this.chave;
+        }
+        public int[] getEstrelas()
+        {
+            return this.estrelas;
+        }
+
+        public int[] lerChave()
+        {
+            Console.WriteLine("Digite o 1º valor:");
+            this.chave[0] = lerNum1aN(50);
+            for (int i = 1; i<5; i++)
+            {
+                Console.WriteLine("Digite o " + (i+1) + "º valor:" );
+                this.chave[i] = lerNum1aN(50);
+                for (int j = 0; j < i; j++)
+                {
+                    while(this.chave[i] == this.chave[j])
+                    {
+                        Console.WriteLine("Número repitido, digite outro valor para a " + (i+1) + "º: ");
+                        this.chave[i] = lerNum1aN(50);
+                    }
+                }
+            }
+            Array.Sort(this.chave);
+            return this.chave;
+        }
+        public void escreverChave()
+        {
+            for(int i = 0; i < this.chave.Length; i++)
+            {
+                Console.WriteLine(i + "º número da chave: " + this.chave[i]);
+            }
+        }
+        
+        public int[] lerEstrelas()
+        {
+            Console.WriteLine("Digite o valor da 1º estrela:");
+            this.estrelas[0] = lerNum1aN(12);
+            Console.WriteLine("Digite o valor da 2º estrela:");
+            this.estrelas[1] = lerNum1aN(12);
+            while (this.estrelas[1] == this.estrelas[0])
+            {
+                Console.WriteLine("Número repitido, digite outro valor para a 2º estrela: ");
+                this.estrelas[1] = lerNum1aN(12);
+            } 
+            Array.Sort(this.estrelas);
+            return this.estrelas;
+        }
+        public void escreverEstrelas()
+        {
+            for (int i = 0; i < this.estrelas.Length; i++)
+            {
+                Console.WriteLine("O valor da " + i + "º estrela é: " + this.estrelas[i]);
+            }
+        }
+        
+        public int[] gerarChaveAleatoria()
+        {
+            Random rnd = new Random();
+            this.chave[0] = rnd.Next(1,51);
+            for (int i = 1; i < 5; i++)
+            {
+                this.chave[i] = rnd.Next(1, 51);
+                for (int j = 0; j < i; j++)
+                {
+                    while (this.chave[i] == this.chave[j])
+                    {
+                        this.chave[i] = this.chave[0] = rnd.Next(1, 51);
+                    }
+                }
+            }
+            Array.Sort(this.chave);
+            return this.chave;
+        }
+        public int[] gerarEstrelasAleatoria()
+        {
+            Random rnd = new Random();
+            this.estrelas[0] = rnd.Next(1, 13);
+            this.estrelas[1] = rnd.Next(1, 13);
+            while (this.estrelas[1] == this.estrelas[0])
+            {
+              this.estrelas[1] = rnd.Next(1, 13);
+            }
+            Array.Sort(this.estrelas);
+            return this.estrelas;
+        }
+
+        public override string ToString()
+        {
+            return "Chave: " + this.chave[0] + ", " + this.chave[1] + ", " + this.chave[2] + ", " +  this.chave[3] + ", " +  this.chave[4] + "\nEstrelas: " + this.estrelas[0] + ", " + this.estrelas[1];
+        }
+        static int lerNum1aN(int vMax)
+        {
+            int n = 0;
+            bool flag;
+            do
+            {
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+                    if(n > vMax || n <= 0)
+                        Console.WriteLine("O número deve ser entre 1 e " + vMax + ": ");
+                    flag = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Error - numero inválido, deve ser positivo e entre 1 e " + vMax);
+                    flag = false;
+                }
+            }
+            while (!flag || n <= 0 || n > vMax);
+            return n;
+        }
+
+    }
+}
---
+++ euromilhoes/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euromilhoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao jogo do Euro Milhões - feito por Jeniffer Azevedo!\n");
+            EuroNumeros jogoJogador = new EuroNumeros();
+            Console.WriteLine("Agora é o momento de fazer o seu jogo!");
+            Console.WriteLine("Escolha 5 numeros entre 1 e 50");
+            jogoJogador.lerChave();
+            Console.WriteLine("Escolha 2 valores entre 1 e 12 para as estrelas");
+            jogoJogador.lerEstrelas();
+            Console.WriteLine("\nSeu jogo é: ");
+            Console.WriteLine(jogoJogador.ToString());
+            EuroNumeros jogoAleatorio = new EuroNumeros();
+            jogoAleatorio.gerarChaveAleatoria();
+            jogoAleatorio.gerarEstrelasAleatoria();
+            Console.WriteLine("\n\nPreparado para ver o resultado?!");
+            Console.Write("Chaves: ");
+            int[] acertosChave = comparar(jogoJogador.getChave(), jogoAleatorio.getChave(), "chave");
+            Console.Write("Estrelas: ");
+            int[] acertosEstrelas = comparar(jogoJogador.getEstrelas(), jogoAleatorio.getEstrelas(), "estrela");
+
+            Console.WriteLine("\nCurioso para saber o jogo sorteado? Ele é:");
+            Console.WriteLine(jogoAleatorio.ToString());
+
+
+            Console.ReadKey();
+        }
+        static int[] comparar(int[] vec1, int[] vec2, string tipo)
+        {
+            int[] iguais = new int[vec1.Length];
+            int cont = 0;
+            for(int i = 0; i < vec1.Length; i++)
+            {
+                for(int j = 0; j < vec1.Length; j++)
+                {
+                    if(vec1[i] == vec2[j])
+                    {
+                        Console.WriteLine("Numero acertado " + vec1[i]);
+                        iguais[i] = vec1[i];
+                        cont++;
+                    }
+                }
+            }
+            Array.Sort(iguais);
+            if (cont == 0)
+            {
+                Console.WriteLine("você não acertou nenhum número da " + tipo + "! Melhor sorte na proxima vez!");
+            }
+            else if (cont == vec1.Length)
+            {
+                Console.WriteLine("Seu dia de sorte!!! Você ganhou!! Aqui está seu 'Parabéns'!");
+            }
+            return iguais;
+        }
+        static void escreverArray(int[] vec1)
+        {
+            for (int i = 0; i < vec1.Length; i++)
+            {
+                Console.WriteLine(vec1[i]);
+            }
+        }
+    }
+}
